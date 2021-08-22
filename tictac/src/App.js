972940731e273ba253b8ac0be11380c383d6b4e1
johnmogi <anguru@gmail.com>
2021-08-22 10:44:11 +0300

    prt

--- tictac/src/App.js
+++ tictac/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to React Parcel Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </div>
+  );
+};
+export default App;
